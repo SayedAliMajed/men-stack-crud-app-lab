b1600044d0c1dda377d843ffad3a38bc9fbc3712
SayedAli <sayed.ali.majed@gmail.com>
2025-09-09 15:04:32 +0300

    Homepage completed

--- models/cars.js
+++ models/cars.js
@@ -2,8 +2,11 @@ const { default: mongoose, model } = require("mongoose");
 
 const carsSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  description: { type: String, required: true },
+  Brand: String,
+  Year: Date,
+  color:String,
   image: String,
+  description: { type: String, required: true },
 });
 
 const Cars = mongoose.model('Cars', carsSchema);
